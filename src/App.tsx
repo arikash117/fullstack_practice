a730a56e7421548263f5ab7e7ed71fe8cfe9264e
Arina <ari.kash117@gmail.com>
2026-03-18 19:27:00 +0300

    event-loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
-import { ContextPage } from './pages/ContextPage';
-import { ReduxPage } from './pages/ReduxPage';
+import HomePage from './HomePage.tsx'
 
 function App() {
+  
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/context" element={<ContextPage />} />
-        <Route path="/redux" element={<ReduxPage />} />
       </Routes>
     </BrowserRouter>
   );
